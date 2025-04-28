5f22f467b07b477849430d886ed11775a3bbeaac
Placewith5s <qsenkaisu@gmail.com>
2025-04-28 18:31:09 +0200

    Fixes
    - Added debounces

--- script.js
+++ script.js
@@ -24,10 +24,16 @@
             }
 
             append_to_display() {
+                let debounce_target_btn = false;
                 this.target_buttons.forEach(btn => {
                     if (btn) {
                         console.log("target btn found")
                         btn.addEventListener("click", () => {
+                            if (debounce_target_btn) {
+                                return
+                            }
+                            debounce_target_btn = true;
+
                             if (this.display.textContent === "" && ["+", "*", "/", ".", "="].includes(btn.textContent.trim())) {
                                 return
                             }
@@ -35,6 +41,10 @@
                                 return
                             }
                             this.display.textContent += btn.textContent
+
+                            setTimeout(() => {
+                                debounce_target_btn = false
+                            }, 100)
                         }, { passive: true })
                     } else {
                         console.warn("No target btn found!")
@@ -42,17 +52,43 @@
                 })
             }
             clear_display() {
+                let debounce_clear_btn = false;
                 this.clear_btn.addEventListener("click", () => {
+                    if (debounce_clear_btn) {
+                        return
+                    }
+                    debounce_clear_btn = true;
+
                     this.display.textContent = ""
+                    
+                    setTimeout(() => {
+                        debounce_clear_btn = false
+                    }, 100)
                 }, { passive: true })
             }
             clear_last_input() {
+                let debounce_last_input_btn = false;
                 this.clear_last_input_btn.addEventListener("click", () => {
+                    if (debounce_last_input_btn) {
+                        return
+                    }
+                    debounce_last_input_btn = true;
+
                     this.display.textContent = this.display.textContent.slice(0, -1)
+
+                    setTimeout(() => {
+                        debounce_last_input_btn = false
+                    }, 100)
                 }, {passive: true })
             }
             calculate() {
+                let debounce_equal_sign_btn = false;
                 this.equal_sign_btn.addEventListener("click", () => {
+                    if (debounce_equal_sign_btn) {
+                        return
+                    }
+                    debounce_equal_sign_btn = true;
+
                     if (this.display.textContent.endsWith("=")) {
                         this.display.textContent = this.display.textContent.slice(0, -1)
                     }
@@ -65,6 +101,10 @@
                         this.display.textContent = "error";
                         console.error(err)
                     }
+
+                    setTimeout(() => {
+                        debounce_equal_sign_btn = false
+                    }, 100)
                 }, { passive: true })
             }
         }
